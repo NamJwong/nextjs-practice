1f8bc28c2288cb3cd094a1ad250828f6b6d8dd04
jynam <njy1007@gmail.com>
2022-03-29 20:19:40 +0900

    useRouter 써보기

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+      <Link href="/">
+        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+      </Link>
+      <Link href="/about">
+        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+          About
+        </a>
+      </Link>
     </nav>
   );
 }
