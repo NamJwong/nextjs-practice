3f22fb99007b0b8f9109968d8c32d121f3f61df0
jynam <njy1007@gmail.com>
2022-04-05 03:32:26 +0900

    _app 컴포넌트를 이용해서 전역 스타일링 하기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import { AppProps } from "next/app";
+
 import NavBar from "../components/NavBar";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavBar />
       <Component {...pageProps} />
+      <style jsx global>
+        {`
+          a {
+            color: #ffb7b7;
+          }
+        `}
+      </style>
     </>
   );
 }
