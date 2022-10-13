04915a93eb5712631bfd57230995219c45623eee
jynam <njy1007@gmail.com>
2022-10-13 15:14:41 +0900

    fix: ga 연결 방식 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,22 +3,27 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-
-        {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-          `,
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
           }}
         />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
